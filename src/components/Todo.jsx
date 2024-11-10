0da0db9e6e7368499b311a1fdc3cb924bc42fe2b
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2024-11-10 10:58:53 +0530

    feat: add new features to form,todo and todolist components

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { CiEdit } from 'react-icons/ci';
+import { FaTrashAlt } from 'react-icons/fa';
 
-const Todo = () => {
+const Todo = ({ task, deleteTodo, editTodo }) => {
   return (
-    <div className='flex justify-between items-center bg-violet-800
-    text-white py-3 px-4 rounded-md mb-1 cursor-pointer'>
-      <p className='font-primary'>Go to University</p>
-      <div>
-        
+    <div
+      className="flex justify-between items-center bg-gray-800 text-white py-3 px-4 rounded-md mb-2 cursor-pointer width-50"
+    >
+      <p className="font-primary">{task.task}</p>
+      <div className="flex items-center gap-x-4">
+        <CiEdit className="text-xl" onClick={() => editTodo(task.id)} />
+        <FaTrashAlt className="text-xl" onClick={() => deleteTodo(task.id)} />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
 
 
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from 'react'
 import Form from './Form'
-import {v4 as uuidv4} from 'uuid'
-uuidv4()
-
-
-
+import { v4 as uuidv4 } from 'uuid'
+import Todo from './Todo'
 
 const TodoList = () => {
-  const [todoValue, setTodo] = useState('');
+  const [todoValue, setTodo] = useState([])
 
-  const createTodo = todo => {
-    setTodo([...todoValue,{id: uuidv4(), task:todo, isEditing:false}])
-  }
+  const createTodo = (todo) => {
+    setTodo([...todoValue, { id: uuidv4(), task: todo, isEditing: false }])
+  };
+
+  const deleteTodo = (id) => {
+    setTodo(todoValue.filter((todo) => todo.id !== id))
+  };
+
+  const editTodo = (id) => {
+    setTodo(todoValue.map((todo) => (todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo)))
+  };
 
   return (
-    <div className='container bg-gray-700 mt-20 p-8 mr-10 rounded-md mx-auto '>
-       <Form createTodo={createTodo}/>
-      </div>
-  )
-}
+    <div className="container bg-gray-700 mt-20 p-8 mr-10 rounded-md mx-auto">
+      <Form createTodo={createTodo} />
+      {Array.isArray(todoValue) &&
+        todoValue.map((todo, index) => (
+          <Todo task={todo} key={index} deleteTodo={deleteTodo} editTodo={editTodo} />
+        ))}
+    </div>
+  );
+};
 
 export default TodoList
+
